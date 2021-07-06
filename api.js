4208c60d3ed46303d4bbb25979c8e0fcf5cc1467
Trevor Larson <trevor.lar@gmail.com>
2021-07-06 12:42:36 -0400

    added Login api to api.js

--- api.js
+++ api.js
@@ -3,6 +3,51 @@ var token = require('./createJWT.js');
 exports.setApp = function ( app, client )
 {
 
+  app.post('/api/login', async (req, res, next) => 
+  {
+      // incoming: login, password
+      // outgoing: id, firstName, lastName, error
+    
+     var error = '';
+    
+      const { login, password } = req.body;
+    
+      const db = client.db();
+      const results = await db.collection('Users').find({Login:login,Password:password}).toArray();
+    
+      var id = -1;
+      var fn = '';
+      var ln = '';
+
+      var ret;
+    
+      if( results.length > 0 )
+      {
+        id = results[0].UserId;
+        fn = results[0].FirstName;
+        ln = results[0].LastName;
+
+        try
+        {
+          const token = require("./createJWT.js");
+          ret = token.createToken( fn, ln, id );
+        }
+        catch(e)
+        {
+          ret = {error:e.message};
+        }
+      }
+      else
+      {
+          ret = {error:"Login/Password incorrect"};
+      }
+    
+      res.status(200).json(ret);
+    });
+
+    
+    
+
     app.post('/api/addDnD', async (req, res, next) =>
     {
       // incoming: userId, color
