4c326bf17c555f787ec23d48d0615f556bce60af
Trevor Larson <trevor.lar@gmail.com>
2021-07-06 22:27:17 -0400

    okay

--- frontend/src/components/Path.js
+++ frontend/src/components/Path.js
@@ -2,13 +2,15 @@ const app_name = 'dndpagemaker'
 exports.buildPath = 
 function buildPath(route)
 {
+    console.log(process.env.NODE_ENV);
     if (process.env.NODE_ENV === 'production') 
     {
         return 'https://' + app_name +  '.herokuapp.com/' + route;
     }
     else
     {        
-        return 'http://localhost:5000/' + route;
+        //return 'http://localhost:5000/' + route;
+        return 'https://' + app_name +  '.herokuapp.com/' + route;
     }
 }
 
