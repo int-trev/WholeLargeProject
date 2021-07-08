14e53def9e605598283597a0a6ba39bd306d6359
int-trev <trevor.lar@gmail.com>
2021-07-08 02:13:29 -0400

    please again

--- frontend/src/components/Path.js
+++ frontend/src/components/Path.js
@@ -2,7 +2,7 @@ const app_name = 'dndpagemaker'
 exports.buildPath = 
 function buildPath(route)
 {
-    console.log(process.env.NODE_ENV);
+    console.log(process.env.MONGODB_URI);
     if (process.env.NODE_ENV === 'production') 
     {
         return 'https://' + app_name +  '.herokuapp.com/' + route;
