694d9a236776a3380d9d742cbed6642051fff703
int-trev <trevor.lar@gmail.com>
2021-07-08 01:17:31 -0400

    please

--- api.js
+++ api.js
@@ -3,11 +3,62 @@ var token = require('./createJWT.js');
 exports.setApp = function ( app, client )
 {
 
-  app.post('/api/login', async (req, res, next) => 
-  {
+    app.post('/api/addcard', async (req, res, next) =>
+    {
+      // incoming: userId, color
+      // outgoing: error
+        
+      const { userId, card, jwtToken } = req.body;
+
+      try
+      {
+        if( token.isExpired(jwtToken))
+        {
+          var r = {error:'The JWT is no longer valid', jwtToken: ''};
+          res.status(200).json(r);
+          return;
+        }
+      }
+      catch(e)
+      {
+        console.log(e.message);
+      }
+    
+      const newCard = {Card:card,UserId:userId};
+      var error = '';
+    
+      try
+      {
+        const db = client.db();
+        const result = db.collection('Cards').insertOne(newCard);
+      }
+      catch(e)
+      {
+        error = e.toString();
+      }
+    
+      var refreshedToken = null;
+      try
+      {
+        refreshedToken = token.refresh(jwtToken).accessToken;
+      }
+      catch(e)
+      {
+        console.log(e.message);
+      }
+    
+      var ret = { error: error, jwtToken: refreshedToken };
+      
+      res.status(200).json(ret);
+    });
+    
+    app.post('/api/login', async (req, res, next) => 
+    {
       // incoming: login, password
       // outgoing: id, firstName, lastName, error
-      console.log("HIT HERE");
+      const aaaa = process.env.MONGODB_URI;
+      console.log(aaaa);
+    
      var error = '';
     
       const { login, password } = req.body;
@@ -44,9 +95,55 @@ exports.setApp = function ( app, client )
     
       res.status(200).json(ret);
     });
+    
+    app.post('/api/searchcards', async (req, res, next) => 
+    {
+      // incoming: userId, search
+      // outgoing: results[], error
+    
+      var error = '';
+    
+      const { userId, search, jwtToken } = req.body;
 
+      try
+      {
+        if( token.isExpired(jwtToken))
+        {
+          var r = {error:'The JWT is no longer valid', jwtToken: ''};
+          res.status(200).json(r);
+          return;
+        }
+      }
+      catch(e)
+      {
+        console.log(e.message);
+      }
+      
+      var _search = search.trim();
+      
+      const db = client.db();
+      const results = await db.collection('Cards').find({"Card":{$regex:_search+'.*', $options:'r'}}).toArray();
+      
+      var _ret = [];
+      for( var i=0; i<results.length; i++ )
+      {
+        _ret.push( results[i].Card );
+      }
+      
+      var refreshedToken = null;
+      try
+      {
+        refreshedToken = token.refresh(jwtToken).accessToken;
+      }
+      catch(e)
+      {
+        console.log(e.message);
+      }
     
-    
+      var ret = { results:_ret, error: error, jwtToken: refreshedToken };
+      
+      res.status(200).json(ret);
+    });
 
     app.post('/api/addDnD', async (req, res, next) =>
     {
@@ -217,7 +314,8 @@ exports.setApp = function ( app, client )
       var ret = { error: error, jwtToken: refreshedToken };
       
       res.status(200).json(ret);
-    });    
+    });
+
     app.post('/api/searchDnD', async (req, res, next) => 
     {
       // incoming: userId, search
--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(bodyParser.json());
 
 require('dotenv').config();
 const url = process.env.MONGODB_URI;
+console.log(url);
 const MongoClient = require('mongodb').MongoClient;
 const client = new MongoClient(url);
 client.connect();
