ddfe657dc616267bf26d786f14961ec98fba7bcf
int-trev <trevor.lar@gmail.com>
2021-07-09 15:38:36 -0400

    api changes

--- api.js
+++ api.js
@@ -284,5 +284,47 @@ app.post('/api/addUser', async (req, res, next) =>
 
     
             });
+
+            app.post('/api/login', async (req, res, next) => 
+            {
+              // incoming: login, password
+              // outgoing: id, firstName, lastName, error
+            
+             var error = '';
+            
+              const { login, password } = req.body;
+            
+              const db = client.db();
+              const results = await db.collection('Users').find({Login:login,Password:password}).toArray();
+            
+              var id = -1;
+              var fn = '';
+              var ln = '';
+        
+              var ret;
+            
+              if( results.length > 0 )
+              {
+                id = results[0].UserId;
+                fn = results[0].FirstName;
+                ln = results[0].LastName;
+        
+                try
+                {
+                  const token = require("./createJWT.js");
+                  ret = token.createToken( fn, ln, id );
+                }
+                catch(e)
+                {
+                  ret = {error:e.message};
+                }
+              }
+              else
+              {
+                  ret = {error:"Login/Password incorrect"};
+              }
+            
+              res.status(200).json(ret);
+            });
     
 }
