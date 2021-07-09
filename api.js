d00574b144899863b24d5429c46e7c9e78bb532e
int-trev <trevor.lar@gmail.com>
2021-07-09 16:33:44 -0400

    Hope

--- api.js
+++ api.js
@@ -229,7 +229,7 @@ exports.setApp = function ( app, client )
         var error = '';  
         
         const { username, password } = req.body;  
-        const newUser = {userName:username,passWord:password,verification:false };
+        const newUser = {Login:username,Password:password,verification:false };
         const db = client.db();
 
         const results = await db.collection('Users').find({Login:username}).toArray();
@@ -257,46 +257,46 @@ exports.setApp = function ( app, client )
     });
           
 
-            app.post('/api/login', async (req, res, next) => 
-            {
-              // incoming: login, password
-              // outgoing: id, firstName, lastName, error
-            
-             var error = '';
-            
-              const { login, password } = req.body;
-            
-              const db = client.db();
-              const results = await db.collection('Users').find({Login:login,Password:password}).toArray();
-            
-              var id = -1;
-              var fn = '';
-              var ln = '';
-        
-              var ret;
-            
-              if( results.length > 0 )
-              {
-                id = results[0].UserId;
-                fn = results[0].FirstName;
-                ln = results[0].LastName;
-        
-                try
-                {
-                  const token = require("./createJWT.js");
-                  ret = token.createToken( fn, ln, id );
-                }
-                catch(e)
-                {
-                  ret = {error:e.message};
-                }
-              }
-              else
-              {
-                  ret = {error:"Login/Password incorrect"};
-              }
-            
-              res.status(200).json(ret);
-            });
+    app.post('/api/login', async (req, res, next) => 
+    {
+      // incoming: login, password
+      // outgoing: id, firstName, lastName, error
+    
+      var error = '';
+    
+      const { login, password } = req.body;
+    
+      const db = client.db();
+      const results = await db.collection('Users').find({Login:login,Password:password}).toArray();
+    
+      var id = -1;
+      var fn = '';
+      var ln = '';
+
+      var ret;
+    
+      if( results.length > 0 )
+      {
+        id = results[0].UserId;
+        fn = results[0].FirstName;
+        ln = results[0].LastName;
+
+        try
+        {
+          const token = require("./createJWT.js");
+          ret = token.createToken( fn, ln, id );
+        }
+        catch(e)
+        {
+          ret = {error:e.message};
+        }
+      }
+      else
+      {
+          ret = {error:"Login/Password incorrect"};
+      }
+    
+      res.status(200).json(ret);
+    });
     
 }
